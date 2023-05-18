7256da5e441278d7eae180686262dbe06c4eb32d
kstates <kathleen.states@gmail.com>
2023-05-18 15:22:13 -0700

    Updating some naming for scenes, and adding a scene changer to the geo safari intro scene.

--- Assets/Scripts/GeoSafari/GeoSafari.cs
+++ Assets/Scripts/GeoSafari/GeoSafari.cs
@@ -1,29 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class GeoSafari : MonoBehaviour
 {
     public TextAsset randomGreetingsFile; 
+    public Button enterGameButton;
     private Greetings greetings;
     private int greetingsLen;
+    private SceneChanger sceneChanger;
 
     // Start is called before the first frame update
     void Start()
     {
+        sceneChanger = gameObject.AddComponent<SceneChanger>();
+
         greetings = JsonUtility.FromJson<Greetings>(randomGreetingsFile.text);
         greetingsLen = greetings.greetings.Length;
 
         float randomGreeting = Random.Range(0, greetingsLen);
     
         GetComponentInChildren<TextMeshProUGUI>().text = greetings.greetings[(int)randomGreeting].greeting;
+        enterGameButton = GetComponentInChildren<Button>();
+
+        enterGameButton.onClick.AddListener(EnterGame);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EnterGame()
     {
-        
-    }
+        sceneChanger.ChangeScene("GeoSafariGame");
+    } 
 }
