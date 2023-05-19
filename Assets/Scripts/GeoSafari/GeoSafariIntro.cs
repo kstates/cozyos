493f3ecc11c4783faa36cfe7d208ed41ef88c396
kstates <kathleen.states@gmail.com>
2023-05-19 11:23:19 -0700

    Moving around geosafari questions and changing names to correctly reflect classes and scenes.

--- Assets/Scripts/GeoSafari/GeoSafariIntro.cs
+++ Assets/Scripts/GeoSafari/GeoSafariIntro.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class GeoSafari : MonoBehaviour
+public class GeoSafariIntro : MonoBehaviour
 {
     public TextAsset randomGreetingsFile; 
     public Button enterGameButton;
